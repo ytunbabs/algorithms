0b9b65f4b9c79c75b4e53859b2dd61515f5f4ca1
Tunde Babayemi <totunde@hotmail.com>
2022-02-01 07:32:38 -0500

    created project.;

---
+++ Algorithms/MergeSort/Interfaces/ISort.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Algorithms.MergeSort.Interfaces
+{
+    public interface ISort
+    {
+        void Sort<T>(T[] sortList, int start, int end);
+    }
+}
---
+++ Algorithms/MergeSort/MergeSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Algorithms.MergeSort.Interfaces;
+
+namespace Algorithms.MergeSort
+{
+    public class MergeSort<T> : ISort
+    {
+        public MergeSort()
+        {
+        }
+
+        public void Sort<T1>(T1[] sortList, int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
